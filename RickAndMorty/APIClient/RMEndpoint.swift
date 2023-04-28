38f3946aa2a336682c5c1bfe5c01fee56d19e21d
arnursakenov <arnur.s@mail.ru>
2023-04-28 17:52:56 +0600

    Models

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,13 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Arnur Sakenov on 28.04.2023.
+//
+
+import Foundation
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,12 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Arnur Sakenov on 28.04.2023.
+//
+
+import Foundation
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String 
+}
